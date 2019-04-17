29a9ba3bf30647174dbecc941545b03dcf219800
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-04-17 15:32:37 -0400

    removed depricated projects

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+$(window).scroll(function() {
+
+    var hT = $('#power-show').offset().top,
+    hH = $('#power-show').outerHeight(),
+    wH = $(window).height(),
+    wS = $(this).scrollTop();
+	if (wS > (hT+hH-wH)){
+		$('#power-hide').fadeIn(500);
+    }
+
+    var hT = $('#easy-show').offset().top,
+    hH = $('#easy-show').outerHeight(),
+    wH = $(window).height(),
+    wS = $(this).scrollTop();
+	if (wS > (hT+hH-wH)){
+	   	$('#easy-hide').fadeIn(500);
+    }
+
+    var hT = $('#father-show').offset().top,
+    hH = $('#father-show').outerHeight(),
+    wH = $(window).height(),
+    wS = $(this).scrollTop();
+	if (wS > (hT+hH-wH)){
+		$('#father-hide').fadeIn(500);
+    }
+
+    var hT = $('#pool-show').offset().top,
+    hH = $('#pool-show').outerHeight(),
+    wH = $(window).height(),
+    wS = $(this).scrollTop();
+	if (wS > (hT+hH-wH)){
+		$('#pool-hide').fadeIn(500);
+    }
+
+    var hT = $('#react-show').offset().top,
+    hH = $('#react-show').outerHeight(),
+    wH = $(window).height(),
+    wS = $(this).scrollTop();
+    if (wS > (hT+hH-wH)){
+        $('#react-hide').fadeIn(500);
+    }
+});
