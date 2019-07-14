b79a303a1e0562916395c1c29743316ae752772b
William Harpley <williamharpley@Williams-MBP.fios-router.home>
2019-07-14 07:11:05 -0400

    removed PURG site from projects

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,5 @@
 $(window).scroll(function() {
 
-    var hT = $('#power-show').offset().top,
-    hH = $('#power-show').outerHeight(),
-    wH = $(window).height(),
-    wS = $(this).scrollTop();
-	if (wS > (hT+hH-wH)){
-		$('#power-hide').fadeIn(500);
-    }
-
-    var hT = $('#easy-show').offset().top,
-    hH = $('#easy-show').outerHeight(),
-    wH = $(window).height(),
-    wS = $(this).scrollTop();
-	if (wS > (hT+hH-wH)){
-	   	$('#easy-hide').fadeIn(500);
-    }
-
     var hT = $('#father-show').offset().top,
     hH = $('#father-show').outerHeight(),
     wH = $(window).height(),
@@ -39,4 +23,22 @@ $(window).scroll(function() {
     if (wS > (hT+hH-wH)){
         $('#react-hide').fadeIn(500);
     }
+
+/*
+    var hT = $('#power-show').offset().top,
+    hH = $('#power-show').outerHeight(),
+    wH = $(window).height(),
+    wS = $(this).scrollTop();
+	if (wS > (hT+hH-wH)){
+		$('#power-hide').fadeIn(500);
+    }
+
+    var hT = $('#easy-show').offset().top,
+    hH = $('#easy-show').outerHeight(),
+    wH = $(window).height(),
+    wS = $(this).scrollTop();
+	if (wS > (hT+hH-wH)){
+	   	$('#easy-hide').fadeIn(500);
+    }
+*/
 });
